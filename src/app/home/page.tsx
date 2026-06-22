61e961c8ef40ffb81f94b8d0d9081ff8ba274397
im-shikha <shikharajput1224@gmail.com>
2026-06-22 09:40:46 +0530

    Image optimization

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,17 +1,39 @@
 import { inter } from "@/app/ui/fonts";
+import Image from "next/image";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col bg-red-100 p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg p-4 md:h-52"></div>
+    <main className="flex min-h-screen flex-col p-6">
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg px-6 py-10 md:w-2/5 md:px-20">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <h1
+            className={`${inter.className} text-4xl font-bold text-white-900 dark:text-black`}
+          >
+            Hello, Next.js!
+          </h1>
           <p
             className={`${inter.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
             <strong>CashBook App</strong> manages your daily expenses and
             income.
           </p>
+        </div>
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
